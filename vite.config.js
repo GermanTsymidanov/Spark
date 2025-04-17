d6b965ab8450e012abceec8d7b36c16ef53a1cd1
GermanTsymidanov <german151207@gmail.com>
2025-04-17 21:46:48 +1000

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,32 @@
 import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
-import {
-    defineConfig
-} from 'vite';
+import { defineConfig } from 'vite';
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.jsx'],
-            ssr: 'resources/js/ssr.jsx',
-            refresh: true,
-        }),
-        react(),
-        tailwindcss(),
-    ],
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.jsx'],
+      ssr: 'resources/js/ssr.jsx',
+      refresh: true,
+    }),
+    react(),
+    tailwindcss(),
+  ],
 
-    server: {
-        port: 5173,
-        host: '0.0.0.0',
-        hmr: {
-            host: 'spark.localhost'
-        },
+  server: {
+    port: 5173,
+    host: '0.0.0.0',
+    hmr: {
+      host: 'spark.localhost'
     },
+  },
 
-    esbuild: {
-        jsx: 'automatic',
-    },
+  esbuild: {
+    jsx: 'automatic',
+  },
 
-    build: {
-     outDir: 'dist',
-   },
+  build: {
+    outDir: 'public/dist',
+  },
 });
